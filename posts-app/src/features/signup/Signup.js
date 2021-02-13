d6d58e5a1685f41f13531dd84ddc7da7e5581067
danyabdo <danyabdo94@gmail.com>
2021-02-13 23:27:08 +0200

    Delete signup icon

--- posts-app/src/features/signup/Signup.js
+++ posts-app/src/features/signup/Signup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from "../../atoms/button"
 import Container from '../../atoms/container';
 import Input from '../../atoms/input';
-import { ReactComponent as SignupIcon } from "../../icons/signup.svg";
+import { ReactComponent as SignupIcon } from "../../icons/login.svg";
 
 export function Signup() {
   const signupButton = "transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block";
