2399c5215d47bb922f5a80ba87befee5d7bf0502
danyabdo <danyabdo94@gmail.com>
2021-02-13 21:46:04 +0200

    Add Routes and signup

--- posts-app/src/App.js
+++ posts-app/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Login } from './features/login/Login';
+import { Signup } from './features/signup/Signup';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <section className="App-header">
-        <Login />
+        <Router>
+          <Switch>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
       </section>
     </div>
   );
---
+++ posts-app/src/features/signup/Signup.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+export function Signup() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <div className="flex flex-row justify-center" >
+          <img alt="logo" src="./assets/logo.png"></img>
+        </div>
+        <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div className="px-5 py-7">
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">Full Name</label>
+            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">Re-Password</label>
+            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+            <button type="button" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+              <span className="inline-block mr-2">Signup</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
