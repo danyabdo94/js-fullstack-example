91ca9208803e3f9f3c7767fed7728b42729aa4ae
danyabdo <danyabdo94@gmail.com>
2021-02-13 15:57:42 +0200

    Add user seed

---
+++ graphql-server/seeders/20210213135227-init.js
@@ -0,0 +1,23 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      name: 'Dany Kamel',
+      email: 'dany@gmail.com',
+      password: await bcrypt.hash("test1234", 10),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
