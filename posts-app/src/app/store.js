34aea331a96aea27170ce83a42e14f9f1275c796
danyabdo <danyabdo94@gmail.com>
2021-02-13 18:08:00 +0200

    Adding identity and login

--- posts-app/src/app/store.js
+++ posts-app/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import LoginReducer from '../features/login/loginSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    Login: LoginReducer,
   },
 });
--- posts-app/src/features/login/loginSlice.js
+++ posts-app/src/features/login/loginSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const LoginSlice = createSlice({
+  name: 'Login',
   initialState: {
     value: 0,
   },
@@ -15,7 +15,7 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = LoginSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
@@ -29,7 +29,7 @@ export const incrementAsync = amount => dispatch => {
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = state => state.counter.value;
+// in the slice file. For example: `useSelector((state) => state.Login.value)`
+export const selectCount = state => state.Login.value;
 
-export default counterSlice.reducer;
+export default LoginSlice.reducer;
